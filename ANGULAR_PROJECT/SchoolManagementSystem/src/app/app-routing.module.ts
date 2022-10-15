e082e92cb106c5f81fa7477f1f5ce8bb763f206c
averma-akash <averma.akash@gmail.com>
2022-10-15 21:16:28 +0530

    Login and Registration Html code

--- ANGULAR_PROJECT/SchoolManagementSystem/src/app/app-routing.module.ts
+++ ANGULAR_PROJECT/SchoolManagementSystem/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './Component/error/error.component';
+import { LoginComponent } from './Component/login/login.component';
+import { RegistrationComponent } from './Component/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : LoginComponent},
+  {path : 'login', component : LoginComponent},
+  {path : 'signup', component : RegistrationComponent},
+  {path : '**', component : ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ANGULAR_PROJECT/SchoolManagementSystem/src/app/app.module.ts
+++ ANGULAR_PROJECT/SchoolManagementSystem/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './Component/registration/registration.component';
+import { LoginComponent } from './Component/login/login.component';
+import { ErrorComponent } from './Component/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
