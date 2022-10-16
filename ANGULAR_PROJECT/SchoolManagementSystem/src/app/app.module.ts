83d8c977ccd5f55a15cff61c9cc974da3baa93bd
averma-akash <averma.akash@gmail.com>
2022-10-16 20:38:19 +0530

    Added configuration

--- ANGULAR_PROJECT/SchoolManagementSystem/src/app/app.module.ts
+++ ANGULAR_PROJECT/SchoolManagementSystem/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './Component/registration/registration.component';
 import { LoginComponent } from './Component/login/login.component';
 import { ErrorComponent } from './Component/error/error.component';
+import { ConfigService } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,9 +18,17 @@ import { ErrorComponent } from './Component/error/error.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide : APP_INITIALIZER,
+      useFactory : (envConfigService : ConfigService) => () =>envConfigService.load().toPromise(),
+      deps : [ConfigService],
+      multi : true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ANGULAR_PROJECT/SchoolManagementSystem/src/environments/environment.ts
+++ ANGULAR_PROJECT/SchoolManagementSystem/src/environments/environment.ts
@@ -2,9 +2,52 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { Injectable } from "@angular/core";
+import { Observable, shareReplay } from "rxjs";
+import { HttpClient, HttpBackend } from '@angular/common/http';
+
 export const environment = {
   production: false
 };
+
+export class Configuration {
+  userLogin : string = '';
+}
+
+export class EnvironmentConstants {
+  public static userLogin : string;
+}
+@Injectable(
+  {providedIn:'root'})
+export class ConfigService {
+
+  private readonly envJson = '../assets/Environment.json';
+
+  private config$ : Observable<Configuration>|any;
+  constructor(private handler : HttpBackend) {}
+  public load() : Observable<Configuration> {
+
+    const backed = new HttpClient(this.handler);
+    console.log(this.config$);
+
+    if(!this.config$) {
+      this.config$ = backed.get<Configuration>(`${this.envJson}`).pipe(shareReplay(1));
+      console.log(this.envJson);
+      this.config$.subscribe({
+        next : (envData: { [x: string]: any; }) => {
+          if(envData && envData[`status`].toLowerCase() === 'success'){
+            const data = envData['data'];
+            EnvironmentConstants.userLogin = data.userLogin;
+          }
+          else {
+            window.alert('Error')
+          }
+        }
+    })
+    }
+    return this.config$
+  }
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
